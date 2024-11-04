a9ad3a0b4e1a91965dacde47720fcac49cb7172c
satriomuslim <satriomuslim2627@gmail.com>
2024-11-04 20:15:55 +0700

    Fixing Single Scan

--- app/src/main/java/com/example/sdklibrary/zebra/SingleScan.kt
+++ app/src/main/java/com/example/sdklibrary/zebra/SingleScan.kt
@@ -3,6 +3,8 @@ package com.example.sdklibrary.zebra
 import android.os.AsyncTask
 import android.util.Log
 import com.zebra.rfid.api3.HANDHELD_TRIGGER_EVENT_TYPE
+import com.zebra.rfid.api3.InvalidUsageException
+import com.zebra.rfid.api3.OperationFailureException
 import com.zebra.rfid.api3.RFIDReader
 import com.zebra.rfid.api3.RfidEventsListener
 import com.zebra.rfid.api3.RfidReadEvents
@@ -10,43 +12,80 @@ import com.zebra.rfid.api3.RfidStatusEvents
 import com.zebra.rfid.api3.STATUS_EVENT_TYPE
 import com.zebra.rfid.api3.TagData
 
-class SingleScan(private val responseHandler: ResponseHandlerInterface) {
+class SingleScan(
+    private val handlerRFID: Connect,
+    private val responseHandler: ResponseHandlerInterface
+) {
 
     private var reader: RFIDReader? = null
 
-    // Read/Status Notify handler
-    // Implement the RfidEventsListener class to receive event notifications
-    inner class EventHandler : RfidEventsListener {
-        private var isTagProcessed = false // Flag to ensure only one TagID is processed
+    fun connectReader() {
+        reader = handlerRFID.reader
+        if (reader == null) {
+            Log.e("Scan", "Reader not connected.")
+        } else {
+            reader?.Events?.addEventsListener(EventHandler())
+        }
+    }
 
+    fun startScan() {
+        try {
+            if (reader != null && reader!!.isConnected) {
+                reader!!.Actions.Inventory.perform()
+            } else {
+                Log.e("Scan", "Reader is null or not connected.")
+            }
+        } catch (e: InvalidUsageException) {
+            e.printStackTrace()
+        } catch (e: OperationFailureException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun stopScan() {
+        try {
+            if (reader != null && reader!!.isConnected) {
+                reader!!.Actions.Inventory.stop()
+            } else {
+                Log.e("Scan", "Reader is null or not connected.")
+            }
+        } catch (e: InvalidUsageException) {
+            e.printStackTrace()
+        } catch (e: OperationFailureException) {
+            e.printStackTrace()
+        }
+    }
+
+    // Read/Status Notify handler
+    // Implement the RfidEventsLister class to receive event notifications
+    inner class EventHandler : RfidEventsListener {
         // Read Event Notification
         override fun eventReadNotify(e: RfidReadEvents) {
             val myTags = reader!!.Actions.getReadTags(100)
-            if (myTags != null && !isTagProcessed) {
-                val firstTag = myTags[0]
-                val tagID = firstTag.tagID
-
-                Log.d(TAG, "Tag ID $tagID RSSI value ${firstTag.peakRSSI}")
-                Log.d(TAG, "RSSI value ${firstTag.peakRSSI}")
-
-                isTagProcessed = true
-
-                AsyncDataUpdate().execute(arrayOf(firstTag))
+            if (myTags != null) {
+                for (index in myTags.indices) {
+                    //  Log.d(TAG, "Tag ID " + myTags[index].getTagID());
+                    Log.d(
+                        TAG,
+                        "Tag ID" + myTags[index].tagID + "RSSI value " + myTags[index].peakRSSI
+                    )
+                    Log.d(TAG, "RSSI value " + myTags[index].peakRSSI)
+                    /* To get the RSSI value*/   //   Log.d(TAG, "RSSI value "+ myTags[index].getPeakRSSI());
+                }
+                AsyncDataUpdate().execute(arrayOf(*myTags))
             }
         }
 
         // Status Event Notification
         override fun eventStatusNotify(rfidStatusEvents: RfidStatusEvents) {
-            Log.d(TAG, "Status Notification: ${rfidStatusEvents.StatusEventData.statusEventType}")
+            Log.d(TAG, "Status Notification: " + rfidStatusEvents.StatusEventData.statusEventType)
             if (rfidStatusEvents.StatusEventData.statusEventType === STATUS_EVENT_TYPE.HANDHELD_TRIGGER_EVENT) {
                 if (rfidStatusEvents.StatusEventData.HandheldTriggerEventData.handheldEvent === HANDHELD_TRIGGER_EVENT_TYPE.HANDHELD_TRIGGER_PRESSED) {
                     object : AsyncTask<Void?, Void?, Void?>() {
                         override fun doInBackground(vararg voids: Void?): Void? {
                             Log.d(TAG, "HANDHELD_TRIGGER_PRESSED")
                             responseHandler.handleTriggerPress(true)
-                            isTagProcessed = false // Reset flag for new tag read
-                            return null
-                        }
+                            return null                        }
                     }.execute()
                 }
                 if (rfidStatusEvents.StatusEventData.HandheldTriggerEventData.handheldEvent === HANDHELD_TRIGGER_EVENT_TYPE.HANDHELD_TRIGGER_RELEASED) {
@@ -54,23 +93,22 @@ class SingleScan(private val responseHandler: ResponseHandlerInterface) {
                         override fun doInBackground(vararg voids: Void?): Void? {
                             responseHandler.handleTriggerPress(false)
                             Log.d(TAG, "HANDHELD_TRIGGER_RELEASED")
-                            return null
-                        }
+                            return null                        }
                     }.execute()
                 }
             }
             if (rfidStatusEvents.StatusEventData.statusEventType === STATUS_EVENT_TYPE.DISCONNECTION_EVENT) {
                 object : AsyncTask<Void?, Void?, Void?>() {
                     override fun doInBackground(vararg voids: Void?): Void? {
-                        // Handle disconnection
-                        return null
+                        TODO("Not yet implemented")
                     }
                 }.execute()
             }
         }
     }
 
-    private inner class AsyncDataUpdate : AsyncTask<Array<TagData?>?, Void?, Void?>() {
+    private inner class AsyncDataUpdate :
+        AsyncTask<Array<TagData?>?, Void?, Void?>() {
         override fun doInBackground(vararg params: Array<TagData?>?): Void? {
             responseHandler.handleTagdata(params[0])
             return null
@@ -80,8 +118,8 @@ class SingleScan(private val responseHandler: ResponseHandlerInterface) {
     interface ResponseHandlerInterface {
         fun handleTagdata(tagData: Array<TagData?>?)
         fun handleTriggerPress(pressed: Boolean)
-        fun barcodeData(`val`: String?)
-        fun sendToast(`val`: String?) //void handleStatusEvents(Events.StatusEventData eventData);
+        fun barcodeData(value: String?)
+        fun sendToast(value: String?)
     }
 
     companion object {
